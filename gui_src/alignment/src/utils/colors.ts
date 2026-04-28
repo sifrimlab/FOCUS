635e38711cf9826371a5d88d82eff450fc61b7eb
Lorenzo Venturelli <venturelli.lorenzo@outlook.it>
2026-04-28 11:54:30 +0200

    Better colors for spot based modalities in alignment gui
    Signed-off-by: Lorenzo Venturelli <venturelli.lorenzo@outlook.it>

--- gui_src/alignment/src/utils/colors.ts
+++ gui_src/alignment/src/utils/colors.ts
@@ -1,9 +1,9 @@
 export const getReferenceColor = (cls: number) => {
-  const hue = (cls * 137.508) % 360;
-  return `hsl(${hue}, 70%, 50%)`;
+  const hue = ((cls * 137.508) % 360) / 360 * 75;  // 0–75° warm (reds, oranges, yellows)
+  return `hsl(${hue}, 60%, 58%)`;
 };
 
 export const getTargetColor = (cls: number) => {
-  const hue = (cls * 137.508 + 180) % 360;
-  return `hsl(${hue}, 70%, 50%)`;
+  const hue = ((cls * 137.508) % 360) / 360 * 75 + 185;  // 185–260° cool (teals, blues, indigos)
+  return `hsl(${hue}, 60%, 58%)`;
 };
